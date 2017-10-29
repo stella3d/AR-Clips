f31bd585de5f58f973a01842d867ab7c841d6daf
Stella Cannefax <stella@unity3d.com>
2017-10-29 06:19:17 -0700

    add basic interfaces

---
+++ Assets/Scripts/ARClips/Interfaces/IMobileARTrackingData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+// based on ARCore data right now, but should cover Kit & Core
+public interface IMobileARTrackingData
+{
+  double timestamp { get; protected set; }
+
+  float lightEstimate { get; protected set; }
+
+  Vector3 devicePosition { get; protected set; }
+  Quaternion deviceRotation { get; protected set; }
+
+  Vector3[] pointCloud { get; protected set; }
+
+  Vector3[][] trackedPlanePolygons { get; protected set; }
+
+  Vector3[] anchorPositions { get; protected set; }
+  Quaternion[] anchorRotations { get; protected set; }
+}
+
