f337db7dfb19fde012ef4cf20821507334337462
Stella Cannefax <stella@unity3d.com>
2017-10-29 08:18:42 -0700

    add light estimate visuals

---
+++ Assets/Scripts/ARClips/Visuals/LightEstimateVisuals.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class LightEstimateVisuals : ARClipVisual
+{
+  public Light light;
+  public float sensitivity = 2f;
+
+  void Update()
+  {
+    light.intensity = m_Reader.lightEstimate * sensitivity;
+  }
+}
+
