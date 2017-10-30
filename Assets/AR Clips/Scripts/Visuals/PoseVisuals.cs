ac0546786aaf9d43176edd9d5b7274d046ba8e03
Stella Cannefax <stella@unity3d.com>
2017-10-29 18:29:34 -0700

    Lerp between positions when using frameskip

--- Assets/AR Clips/Scripts/Visuals/PoseVisuals.cs
+++ Assets/AR Clips/Scripts/Visuals/PoseVisuals.cs
@@ -4,11 +4,26 @@ using System.Collections;
 public class PoseVisuals : ARClipVisual
 {
   public GameObject deviceObject;
+
+  int skipIndex = 1;
 	
   void Update()
   {
-    deviceObject.transform.position = m_Reader.position;
     deviceObject.transform.rotation = m_Reader.rotation;
+
+    var skip = m_Reader.updatesPerDeviceUpdate;
+    if (skip > 1)
+    {
+      var trans = deviceObject.transform;
+
+      trans.position = Vector3.Lerp(trans.position, m_Reader.position, skipIndex / skip);
+      trans.rotation = Quaternion.Lerp(trans.rotation, m_Reader.rotation, skipIndex / skip);
+
+      if (skipIndex < skip)
+        skipIndex++;
+      else if(skipIndex == skip)
+        skipIndex = 1;
+    }
   }
 }
 
