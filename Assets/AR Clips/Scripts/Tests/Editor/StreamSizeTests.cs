41f3c098132521f3dd1625bab2db4f9cc416e1d6
Stella Cannefax <stella@unity3d.com>
2017-10-30 06:45:30 -0700

    add GzipWriter size perf test

---
+++ Assets/AR Clips/Scripts/Tests/Editor/StreamSizeTests.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections;
+
+public class StreamSizeTests 
+{
+  FileStream m_File;
+  FileStream m_GzipFile;
+  BufferedStream m_Buffer;
+  GZipStream m_GzipStream;
+  BinaryWriter m_Writer;
+  BinaryWriter m_GzipWriter;
+
+  [OneTimeSetUp]
+  public void Setup()
+  {
+    var fileName = Application.dataPath + "/uncompressed.txt";
+    var gzipFileName = Application.dataPath + "/compressed.txt";
+
+    m_File = new FileStream(fileName, FileMode.CreateNew);
+    m_GzipFile = new FileStream(gzipFileName, FileMode.CreateNew);
+    m_GzipStream = new GZipStream(m_GzipFile, CompressionMode.Compress);
+    m_Buffer = new BufferedStream(m_GzipStream, 65536);
+
+    m_Writer = new BinaryWriter(m_File);
+    m_GzipWriter = new BinaryWriter(m_Buffer);
+  }
+
+	[Test]
+	public void WriteBothStreams() 
+  {
+    // our real data is mostly lots of floats puncuated by int32s
+    for (float i = 0; i < 1000000; i+= 0.5f)
+    {
+      var x = i / 2f;
+      var y = i / 4f;
+      var z = i / 8f;
+      m_Writer.Write(x);
+      m_Writer.Write(y);
+      m_Writer.Write(z);
+      m_GzipWriter.Write(x);
+      m_GzipWriter.Write(y);
+      m_GzipWriter.Write(z);
+
+      if (i % 100 == 0)
+      {
+        m_Writer.Write((int) i / 100);
+        m_Writer.Write(';');
+        m_GzipWriter.Write((int) i / 100);
+        m_GzipWriter.Write(';');
+      }
+    }
+
+    m_Writer.Flush();
+    m_GzipWriter.Flush();
+    m_Writer.Close();
+    m_GzipWriter.Close();
+    m_File.Close();
+    m_GzipFile.Close();
+	}
+    
+}
