459862b9142adc915bd78f1552af3e54feb1c287
Stella Cannefax <stella@unity3d.com>
2017-11-03 06:15:07 -0700

    remove convex hull stuff for now

--- Assets/AR Clips/Scripts/Visuals/PointCloudVisuals.cs
+++ Assets/AR Clips/Scripts/Visuals/PointCloudVisuals.cs
@@ -56,47 +56,5 @@ public class PointCloudVisuals : ARClipVisual
       previousPointCount = m_Reader.pointCount;
   }
 
-  void CreateSelection(IEnumerable<Vector3> points)
-     {
-         var selection = new GameObject("Selection");
-         MeshFilter meshFilter = (MeshFilter)selection.AddComponent(typeof(MeshFilter));
- 
-         meshFilter.mesh = CreateMesh(points);
- 
-         MeshRenderer renderer = selection.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-         renderer.material.shader = Shader.Find("Particles/Additive");
-         Texture2D tex = new Texture2D(1, 1);
-         tex.SetPixel(0, 0, Color.green);
-         tex.Apply();
-         renderer.material.mainTexture = tex;
-         renderer.material.color = Color.green;
-     }
-
-     List<int> m_Triangles = new List<int>(1000);
- 
-     Mesh CreateMesh(IEnumerable<Vector3> stars)
-     {
-         Mesh m = new Mesh();
-         m.name = "ScriptedMesh";
-         m_Triangles.Clear();
- 
-         var vertices = stars.Select(x => new Vertex(x)).ToList();
- 
-         var result = MIConvexHull.ConvexHull.Create(vertices);
-         m.vertices = result.Points.Select(x => x.ToVec()).ToArray();
-         var xxx = result.Points.ToList();
- 
-         foreach(var face in result.Faces)
-         {
-             m_Triangles.Add(xxx.IndexOf(face.Vertices[0]));
-             m_Triangles.Add(xxx.IndexOf(face.Vertices[1]));
-             m_Triangles.Add(xxx.IndexOf(face.Vertices[2]));
-         }
- 
-         m.triangles = m_Triangles.ToArray();
-         m.RecalculateNormals();
-         return m;
-     }
-
 }
 
